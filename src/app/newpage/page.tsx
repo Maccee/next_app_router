17f882ebd5ea6100bc5dd02b68cb934e7b93ae09
Maccee <makkeeh@gmail.com>
2024-02-19 17:56:25 +0200

    leaflet

--- src/app/newpage/page.tsx
+++ src/app/newpage/page.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
 
-import Image from "next/image";
 
 const Home = () => {
   return (
-    <main className="p-2 w-[1440px] mx-auto border-2 h-[100vh]">
+    <main className="p-2 max-w-[1440px] mx-auto border-2">
       This is newpage
-      <Image src="/flo.jpg" alt="image" width={300}  height={0} />
+     
 
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-const Home = () => {
+import "leaflet/dist/leaflet.css";
+import dynamic from 'next/dynamic';
+
+const MapComponentWithNoSSR = dynamic(() => import('./components/mapcomponent'), {
+  ssr: false,
+});
+
+export default function Home() {
   return (
-    <main>
-      This is Home page
-      
+    <main className="h-[300px]">
+      <MapComponentWithNoSSR />
     </main>
   );
-};
-
-export default Home;
+}
